e5bd2adfb2105e479a17a4cf8a273a77a99c6567
Austine Do <austine.do13@gmail.com>
2023-09-04 23:40:47 -0500

    update

--- cs313e_assign_2/intervals.py
+++ cs313e_assign_2/intervals.py
@@ -1,31 +1,60 @@
 """
-    Input: tuples_list is an unsorted list of tuples denoting intervals
-    Output: a list of merged tuples sorted by the lower number of the
-    interval
+  File: spiral.py
+  Description: 
+
+  Student Name: Austine Do
+  Student UT EID: ahd589
+
+  Partner Name: Ahyeon Ko
+  Partner UT EID: ak42464
+
+  Course Name: CS 313E
+  Unique Number: 52590
+  Date Created: 9/3/23
+  Date Last Modified: 9/6/23
 """
+
 import sys
 
 
-def merge_tuples (tuples_list):
-    """Merge the tuples"""
+def merge_tuples(tuples_list):
+    """
+    Input: tuples_list is an unsorted list of tuples denoting intervals
+    Output: a list of merged tuples sorted by the lower number of the
+    interval
+    """
+    if not tuples_list:
+        return []
+    
+    merged_list = []
+    prev_len = len(tuples_list)
+    tuples_list.sort(key=lambda x: x[0])
 
-    # ADD YOUR CODE HERE ... AND REMOVE THIS Line
-    return None # Replace this 
+    while tuples_list:
+        current_interval = tuples_list.pop(0)
+        
+        if not merged_list or current_interval[0] > merged_list[-1][1]: # if the list is empty or the interval is not merge-able
+            merged_list.append(current_interval)
+        else:                                                           # merges the interval [ current_interval[0] <= merged_list[-1][1] ]
+            merged_list[-1] = (merged_list[-1][0], max(current_interval[1], merged_list[-1][1]))
+
+        if len(tuples_list) == prev_len:
+            break
+
+        prev_len = len(tuples_list)
+    
+    return merged_list
 
 
-
-def sort_by_interval_size (tuples_list):
+def sort_by_interval_size(tuples_list):
     """
     Input: tuples_list is a list of tuples of denoting intervals
     Output: a list of tuples sorted by ascending order of the
     size of the interval if two intervals have the size then it will sort by the
     lower number in the interval
     """
-
-    # Add Your Code HERE ... AND REMOVE THIS Line
-
-    return None # Replace this.
-
+    sorted_merged_list = sorted(tuples_list, key=lambda interval: (interval[1]-interval[0], interval[0]))
+    return sorted_merged_list
 
 
 def main():
@@ -46,7 +75,7 @@ def main():
     merged = merge_tuples(tuples_list)
 
     # sort the list of tuples according to the size of the interval
-    sorted_merge = sort_by_interval_size(merge_tuples(tuples_list))
+    sorted_merge = sort_by_interval_size(merged)
 
     # write the output list of tuples from the two functions
     print(merged)
